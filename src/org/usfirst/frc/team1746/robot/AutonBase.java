6b28f7410b114e99c8418fe60060ff83ea821413
rodneyphan <firerachet@gmail.com>
2018-02-01 19:48:50 -0500

    Autonomous Selector Maybe

--- src/org/usfirst/frc/team1746/robot/AutonBase.java
+++ src/org/usfirst/frc/team1746/robot/AutonBase.java
@@ -12,20 +12,19 @@ public class AutonBase {
 	
 	private DriveTrain m_drivetrain;
 	
-	SendableChooser<Double> startSelector = new SendableChooser<>();
-	SendableChooser<Boolean> scaleFirstSelector = new SendableChooser<>();
-	SendableChooser<String> switchSelector = new SendableChooser<>();
-	SendableChooser<Boolean> startDelaySelector = new SendableChooser<>();
-	SendableChooser<Boolean> switchDelaySelector = new SendableChooser<>();
-	SendableChooser<Boolean> scaleDelaySelector = new SendableChooser<>();
-	SendableChooser<Boolean> scoreScaleSelector = new SendableChooser<>();
-	SendableChooser<Boolean> scoreSwitchSelector = new SendableChooser<>(); 
+	SendableChooser<Double> startSelector = new SendableChooser<>(); //Send And Use
+	SendableChooser<Boolean> scaleFirstSelector = new SendableChooser<>(); // ?
+	SendableChooser<String> switchSelector = new SendableChooser<>(); //Use
+	SendableChooser<Boolean> startDelaySelector = new SendableChooser<>(); //Send
+	SendableChooser<Boolean> switchDelaySelector = new SendableChooser<>(); //Send
+	SendableChooser<Boolean> scaleDelaySelector = new SendableChooser<>(); //Send
+	SendableChooser<Boolean> scoreScaleSelector = new SendableChooser<>(); //Send
+	SendableChooser<Boolean> scoreSwitchSelector = new SendableChooser<>(); //Send
 	
 	String gameData;
-	char nearSwitchP;
+	char ourSwitchP;
 	char scaleP;
-	char farSwitchP;
-	
+	char oppSwitchP;
 	
 	public AutonBase(DriveTrain driveTrain){
 		m_drivetrain = driveTrain;
@@ -34,11 +33,34 @@ public class AutonBase {
 	
 	AutonConstants aConstants = new AutonConstants();
 	
+	AutonCrossTheLine crtl;
+	AutonDS2ScaleFar dtcf;
+	AutonDS2ScaleNear dtcn;
+	AutonDS2SwichFar dtwf;
+	AutonDS2SwichNear dtwn;
+	AutonNone none;
+	AutonScale2SwitchFar ctwf;
+	AutonScale2SwitchNear ctwn;
+	AutonSwitch2ScaleFar wtcf;
+	AutonSwitch2ScaleNear wtcn;
+	
 	public void init() {
 		gameData = DriverStation.getInstance().getGameSpecificMessage();
-		nearSwitchP = gameData.charAt(0);
+		ourSwitchP = gameData.charAt(0);
 		scaleP = gameData.charAt(1);
-		farSwitchP = gameData.charAt(2);
+		oppSwitchP = gameData.charAt(2);
+		
+		crtl = new AutonCrossTheLine(m_drivetrain);
+//		dtcf = new AutonDS2ScaleFar(m_drivetrain,selectedStart());
+//		dtcn = new AutonDS2ScaleNear(m_drivetrain,selectedStart());
+//		dtwf = new AutonDS2SwichFar(m_drivetrain,selectedStart());
+//		dtwn = new AutonDS2SwichNear(m_drivetrain,selectedStart());
+		none = new AutonNone();
+//		ctwf = new AutonScale2SwitchFar(m_drivetrain,selectedStart());
+//		ctwn = new AutonScale2SwitchNear(m_drivetrain,selectedStart());
+//		wtcf = new AutonSwitch2ScaleFar(m_drivetrain,selectedStart());
+//		wtcn = new AutonSwitch2ScaleNear(m_drivetrain,selectedStart());
+		
 	}
 	
 	public void initSmartDashboard() {
@@ -60,9 +82,9 @@ public class AutonBase {
 		SmartDashboard.putBoolean("Start Delay", selectedStartDelay());
 		SmartDashboard.putBoolean("Switch Delay", selectedSwitchDelay());
 		SmartDashboard.putBoolean("Scale Delay", selectedScaleDelay());
-		SmartDashboard.putString("Near Switch Position", nSwitchPosition());
+		SmartDashboard.putString("Our Switch Position", ourSwitchPosition());
 		SmartDashboard.putString("Scale Position", scalePosition());
-		SmartDashboard.putString("Far Switch Position", fSwitchPosition());
+		SmartDashboard.putString("Opponent Switch Position", oppSwitchPosition());
 		SmartDashboard.putBoolean("Score Scale At End", selectedScoreScale());
 		SmartDashboard.putBoolean("Score Switch At End", selectedScoreSwitch());
 		
@@ -98,14 +120,14 @@ public class AutonBase {
 		return scoreSwitchSelector.getSelected();
 	}
 	
-	public String fSwitchPosition() {
-		String fSwitchPosition = null;
-		if(farSwitchP == 'L') {
-			fSwitchPosition = "Left";
-		}else if(farSwitchP == 'R'){
-			fSwitchPosition = "Right";	
+	public String oppSwitchPosition() {
+		String oppSwitchPosition = null;
+		if(oppSwitchP == 'L') {
+			oppSwitchPosition = "Left";
+		}else if(oppSwitchP == 'R'){
+			oppSwitchPosition = "Right";	
 		}
-		return fSwitchPosition;
+		return oppSwitchPosition;
 	}
 	public String scalePosition() {
 		String scalePosition = null;
@@ -116,14 +138,14 @@ public class AutonBase {
 		}
 		return scalePosition;
 	}
-	public String nSwitchPosition() {
-		String nSwitchPosition = null;
-		if(nearSwitchP == 'L') {
-			nSwitchPosition = "Left";
-		}else if(nearSwitchP == 'R'){
-			nSwitchPosition = "Right";	
+	public String ourSwitchPosition() {
+		String ourSwitchPosition = null;
+		if(ourSwitchP == 'L') {
+			ourSwitchPosition = "Left";
+		}else if(ourSwitchP == 'R'){
+			ourSwitchPosition = "Right";	
 		}
-		return nSwitchPosition;
+		return ourSwitchPosition;
 	}
 	
 	public void initScaleFirstSelector(){
@@ -132,8 +154,9 @@ public class AutonBase {
 		SmartDashboard.putData("Scale First", scaleFirstSelector);
 	}
 	public void initSwitchSelector(){
-		switchSelector.addDefault("Near", "near");
-		switchSelector.addObject("Far", "far");
+		switchSelector.addDefault("None", "none");
+		switchSelector.addObject("Switch", "switch");
+		switchSelector.addObject("cross", "cross");
 		switchSelector.addObject("Scale", "scale");
 		SmartDashboard.putData("Switch Selector", switchSelector);
 	}
@@ -178,8 +201,44 @@ public class AutonBase {
 	}
 	
 	public void run(){
-		
-	}	
-	
-	
+		if(selectedSwitch() == "scale") {
+			if(selectedStart() >= 2) { //If Mid Left Or Left
+				if(scalePosition() == "Left") {
+//					dtcn.auton();
+				} else if (scalePosition() == "Right") {
+//					dtcf.auton();
+				}
+			}else if(selectedStart() <= 3) { // If Mid Right Or Right
+				if(scalePosition() == "Right") {
+//					dtcn.auton();
+				} else if (scalePosition() == "Left") {
+//					dtcf.auton();
+				}
+			}
+		}else if(selectedSwitch() == "switch") {
+			if(selectedStart() >= 2) { //If Mid Left Or Left
+				if(ourSwitchPosition() == "Left") {
+//					dtwn.auton();
+				} else if (ourSwitchPosition() == "Right") {
+//					dtwf.auton();
+				}
+			}else if(selectedStart() <= 3) { // If Mid Right Or Right
+				if(ourSwitchPosition() == "Right") {
+//					dtwn.auton();
+				} else if (ourSwitchPosition() == "Left") {
+//					dtwf.auton();
+				}
+			}
+		}else if(selectedSwitch() == "both") {
+			if(selectedScaleFirst()) {
+//				Scale First
+			}else {
+//				Switch First
+			}
+		}else if(selectedSwitch() == "cross") {
+			crtl.auton();
+		}else{
+//			none.auton();
+		}
+	}		
 }
