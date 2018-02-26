e068a43394d9223923423f9a5a5a163baaa8d40f
FRC Team 1746 B <team1746@gmail.com>
2018-02-26 16:49:32 -0500

    Shooter

---
+++ src/org/usfirst/frc/team1746/robot/Shoot.java
@@ -0,0 +1,30 @@
+package org.usfirst.frc.team1746.robot;
+
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+import edu.wpi.first.wpilibj.Solenoid;
+
+public class Shoot {
+	
+	ElectricalConstants m_eConstants;
+	Controls m_controls;
+	
+	Solenoid m_shootIn;
+	Solenoid m_shootOut;
+	
+	
+	public Shoot(Controls controls) {
+		m_controls = controls;
+		m_eConstants =  new ElectricalConstants();
+		m_shootIn = new Solenoid(m_eConstants.SHOOTIN);
+		m_shootOut = new Solenoid(m_eConstants.SHOOTOUT);
+		}
+	
+	public void shootOut(){
+		m_shootIn.set(false);
+		m_shootOut.set(true);
+	}
+	public void shootIn(){
+		m_shootOut.set(false);
+		m_shootIn.set(true);
+	}
+}
